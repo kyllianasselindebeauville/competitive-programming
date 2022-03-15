9d187d0ede3027a8ac21cc779a1c8ff8ffe0bb97
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-03-15 20:07:47 +0100

    Add template

---
+++ main.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+void solve() {
+  // Code here.
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t, c = 0;
+  cin >> t;
+  while (t--) {
+    cout << "Case #" << ++c << ": ";
+    solve();
+  }
+}
